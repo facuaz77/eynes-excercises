4928b7397375df926ebec91caf78e37477a74f12
facuaz77 <alanizfacundo902@gmail.com>
2025-07-07 22:38:03 -0300

    transpose exercise

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,15 @@
 def transpose(matrix):
-    pass
+
+    transpose = list(zip(*matrix))
+
+    for x in transpose:
+        print(x)
+
+
+matrix = [
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9']
+    ]
+
+transpose(matrix)
