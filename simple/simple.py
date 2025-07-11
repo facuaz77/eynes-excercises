5dad62627e888d2b3cfd1a36207144219cb1012c
facuaz77 <alanizfacundo902@gmail.com>
2025-07-11 14:18:45 -0300

    Test simple 1

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,21 @@
 import random
 
-
 def simple_list():
-    pass
+    users = []
+    for i in range(1, 11):  
+        user = {
+            'id': str(i),
+            'age': random.randint(1, 100)  
+        }
+        users.append(user)
+    return users
 
 
-def sort_list():
-    pass
+def sort_list(dicts):
+    return sorted(dicts, key=lambda user: user['age'])
+    
+
+users = simple_list()
+
+for user in sort_list(users):
+    print(user)
